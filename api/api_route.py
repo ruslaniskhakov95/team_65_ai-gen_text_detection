1b7da7147b077779cb586bae8e1d3dae68a62d66
Ruslan Iskhakov <ruslan.iskhak94@gmail.com>
2024-12-28 12:45:04 +0300

    Fit and load/unload for new models added

--- api/api_route.py
+++ api/api_route.py
@@ -1,44 +1,112 @@
-from fastapi import APIRouter
-import pickle
-import os
+from fastapi import APIRouter, HTTPException
+import numpy as np
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
+from sklearn.linear_model import LogisticRegression
+from sklearn.svm import SVC
 
 from utils import (
-    PredictSingleRequest, PredictMultipleRequest, PredictResponse,
-    tokenize_and_clean_text, lemmatize
+    PredictRequest, LoadRequest, PredictMultipleRequest, PredictResponse,
+    FitRequest, ApiResponse, tokenize_and_clean_text, lemmatize,
+    tfidf_model, tfidf_vec, ModelListResponse
 )
 
 
 router = APIRouter(prefix='/api/v1/model')
+models = {
+    'default': [tfidf_vec, tfidf_model, 'logistic']
+}
+active_model = 'default'
 
 
-current_dir = os.path.dirname(__file__)
-vec_filename = os.path.join(
-    current_dir, '../baseline_OUTFOX/tfidf_vectorizer_uni.pkl'
-)
-with open(vec_filename, 'rb') as vec_file:
-    tfidf_vec = pickle.load(vec_file)
+@router.post("/fit_corpus", response_model=ApiResponse)
+async def fit_new_model(request: FitRequest):
+    '''Train new model'''
+
+    global models
+    config = request.config
+    model_type = config.model_type.value
+    vec_type = config.vec_type.value
+    model_id = config.id
+    params = config.hyperparams.dict()
+    vec_params = config.vec_params.dict()
+    X = request.X
+    y = np.array(request.y)
+    cleaned_texts = []
+    for text in X:
+        cleaned_texts.append(' '.join(
+            lemmatize(tokenize_and_clean_text(text))
+        ))
+
+    if model_type == 'logistic':
+        model = LogisticRegression(**params)
+    elif model_type == 'svm':
+        model = SVC(**params)
+    if vec_type == 'bow':
+        vec = CountVectorizer(**vec_params)
+    elif vec_type == 'tfidf':
+        vec = TfidfVectorizer(**vec_params)
+
+    X_vec = vec.fit_transform(cleaned_texts)
+    model.fit(X_vec, y)
+    models[model_id] = [vec, model, model_type]
+
+    return ApiResponse(
+        message=f'Model {model_id} successfully trained! Vec: {vec_type}'
+    )
 
 
-model_filename = os.path.join(
-    current_dir, '../baseline_OUTFOX/model_log_tfidf.pkl'
-)
-with open(model_filename, 'rb') as model_file:
-    tfidf_model = pickle.load(model_file)
+@router.post("/load", response_model=ApiResponse)
+async def load_model(request: LoadRequest):
+    global active_model
+    global models
+    model_id = request.id
+    if models.get(model_id):
+        active_model = model_id
+    else:
+        raise HTTPException(
+            status_code=404, detail="Requested model doesn't exist"
+        )
+    return ApiResponse(message=f'Model {model_id} successfully loaded')
+
+
+@router.post("/unload", response_model=ApiResponse)
+async def unload_model():
+    global active_model
+    global models
+    if not active_model:
+        raise HTTPException(
+            status_code=404, detail="There's no active loaded model"
+        )
+    temp = active_model
+    active_model = None
+    return ApiResponse(message=f'Model {temp} unloaded!')
+
+
+@router.get("/list", response_model=list[ModelListResponse])
+async def list_models():
+    return [
+        ModelListResponse(
+            id=key, type=value[2]
+        ) for key, value in models.items()
+    ]
 
 
 @router.post("/predict_text", response_model=PredictResponse)
-async def predict_model(request: PredictSingleRequest):
+async def predict_model(request: PredictRequest):
     '''Prediction on a single text'''
 
-    global tfidf_vec
-    global tfidf_model
+    if not active_model:
+        raise HTTPException(
+            status_code=404, detail='No active model, choose one!'
+        )
     text = request.X
     cleaned_text = ' '.join(lemmatize(tokenize_and_clean_text(text)))
     text_list = []
     text_list.append(cleaned_text)
-    text_vec = tfidf_vec.transform(text_list)
-    pred = tfidf_model.predict(text_vec).tolist()
-    print(tfidf_model.predict_proba(text_vec))
+
+    vec, model = models[active_model][0], models[active_model][1]
+    text_vec = vec.transform(text_list)
+    pred = model.predict(text_vec).tolist()
     return PredictResponse(prediction=pred)
 
 
@@ -46,14 +114,17 @@ async def predict_model(request: PredictSingleRequest):
 async def predict_model_corpus(request: PredictMultipleRequest):
     '''Prediction on a text corpus'''
 
-    global tfidf_vec
-    global tfidf_model
+    if not active_model:
+        raise HTTPException(
+            status_code=404, detail='No active model, choose one!'
+        )
     corpus = request.X
     cleaned_texts = []
     for text in corpus:
         cleaned_texts.append(' '.join(
             lemmatize(tokenize_and_clean_text(text))
         ))
-    text_vec = tfidf_vec.transform(cleaned_texts)
-    pred = tfidf_model.predict(text_vec).tolist()
+    vec, model = models[active_model][0], models[active_model][1]
+    text_vec = vec.transform(cleaned_texts)
+    pred = model.predict(text_vec).tolist()
     return PredictResponse(prediction=pred)
