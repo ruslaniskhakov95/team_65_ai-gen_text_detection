ae66f93bb122cd064cc038c00d5394f82a7e7148
Ruslan Iskhakov <ruslan.iskhak94@gmail.com>
2025-01-02 19:01:32 +0300

    try to make sync fit

--- streamlit_app/pages/fit.py
+++ streamlit_app/pages/fit.py
@@ -55,7 +55,7 @@ async def process_page():
                     st.write("Training the model...")
                     try:
                         _, result = await fit_model(config)
-                        time.sleep(20)
+                        print(result)
                         st.success(result["message"])
                         train_sizes = result["train_sizes"]
                         train_scores_mean = result["train_scores_mean"]
